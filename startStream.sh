8f04337f359089cae52ad2f5358347c8c02f8c64
Gavin Morgan <morgan.gavin@gmail.com>
2023-04-21 14:03:40 -0700

    format

--- startStream.sh
+++ startStream.sh
@@ -20,18 +20,18 @@ TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
 # Start streaming the Xvfb buffer to YouTube using ffmpeg and log diagnostics
 ffmpeg -f x11grab \
-	-s 1280x720 \
-	-r 30 \
-	-i $DISPLAY_NUM \
-	-c:v libx264 \
-	-preset veryfast \
-	-b:v 2500k \
-	-maxrate 2500k \
-	-bufsize 5000k \
-	-vf "format=yuv420p" \
-	-g 50 \
-	-f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY" \
-	-loglevel debug 2>&1 | tee "${LOG_DIR}/${TIMESTAMP}_${LOG_FILE}"
+-s 1280x720 \
+-r 30 \
+-i $DISPLAY_NUM \
+-c:v libx264 \
+-preset veryfast \
+-b:v 2500k \
+-maxrate 2500k \
+-bufsize 5000k \
+-vf "format=yuv420p" \
+-g 50 \
+-f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY" \
+-loglevel debug 2>&1 | tee "${LOG_DIR}/${TIMESTAMP}_${LOG_FILE}"
 
 # Clean up background processes when the script exits
 trap "killall cool-retro-term; killall Xvfb" EXIT
