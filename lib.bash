05407eeb6f80109bdcbfb938dc9dbcbbf6399e9a
Gavin Morgan <morgan.gavin@gmail.com>
2023-04-27 20:48:43 -0700

    decrease bit depth

--- lib.bash
+++ lib.bash
@@ -11,23 +11,25 @@ function stream_command() {
     local COMMAND=$3
     local OTHER_ARGS=${@:4}   
 
-    # Start a virtual X server on the specified display, with a resolution of 640x480 and 24-bit color depth.
+    # Start a virtual X server on the specified display, with a resolution of 640x480 and 16-bit color depth.
     # Disable listening on TCP port and set authentication to /dev/null.
-    Xvfb $DISPLAY_NUM -screen 0 640x480x24 -nolisten tcp -auth /dev/null &
+    Xvfb $DISPLAY_NUM -screen 0 640x480x16 -nolisten tcp -auth /dev/null &
     DISPLAY=$DISPLAY_NUM "$COMMAND" $OTHER_ARGS &
     
 
-    # Capture the output of the virtual display and encode it with ffmpeg.
-    # Use annullsrc filter to generate silent audio; audio is necessary to satisfy YouTube.
-    # Use x11grab input device to capture the video output of the virtual display.
-    # Set framerate to 20 frames per second, video bitrate to 1500k, and buffer size to 1500k.
-    # Use libx264 codec to encode the video stream and set the preset to veryslow for better quality.
-    # Stream the output to the specified RTMP URL, the YouTube Live server.
+    
+    # Use annullsrc filter to generate silent audio; 
+    # audio is necessary to satisfy YouTube.
+    # 
+    # -pix_fmt uv420p is a planar YUV format, which means that 
+    # the Y, U, and V components are stored separately.
+    # This is a more efficient format for video encoding, 
+    # as it allows the encoder to compress each component separately.
 
     ffmpeg \
     -f lavfi -i anullsrc -c:a pcm_u8 \
     -f x11grab -video_size 640x480 -draw_mouse 0 -i "$DISPLAY_NUM"+0,0 \
     -framerate 20 -b:v 1500k -maxrate 1500k -bufsize 1500k \
-    -c:v libx264 -preset veryslow \
+    -c:v libx264 -preset veryfast -pix_fmt yuv420p \
     -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
 }
